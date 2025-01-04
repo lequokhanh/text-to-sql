6038f38e7796b2a4d52ab1f64beae5002ae65e4e
21522255 <21522255@ms.uit.edu.vn>
2025-01-04 11:16:02 +0700

    up

---
+++ devops/postgres/register-to-consul.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CONSUL_URL=${CONSUL_HTTP_ADDR}
+POSTGRES_SERVICE_NAME="postgres"
+POSTGRES_PORT=5432
+
+echo "Registering PostgreSQL with Consul at $CONSUL_URL..."
+
+# Register PostgreSQL service
+curl -X PUT "${CONSUL_URL}/v1/agent/service/register" \
+    -H "Content-Type: application/json" \
+    -d "{
+  \"ID\": \"postgres\",
+  \"Name\": \"${POSTGRES_SERVICE_NAME}\",
+  \"Tags\": [\"database\", \"sql\"],
+  \"Address\": \"$(hostname -i)\",
+  \"Port\": ${POSTGRES_PORT},
+  \"Check\": {
+    \"ID\": \"postgresql-check\",
+    \"Name\": \"PostgreSQL TCP Check\",
+    \"TCP\": \"$(hostname -i):${POSTGRES_PORT}\",
+    \"Interval\": \"10s\",
+    \"Timeout\": \"5s\"
+  }
+}"
+
+echo "PostgreSQL registered with Consul."
