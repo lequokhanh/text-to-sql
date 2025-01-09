0e503a8418727afda462e9155e78079b932a12b9
lequokhanh <lequockhanhkt03@gmail.com>
2025-01-09 19:25:03 +0700

    add api query

---
+++ slm-embed/src/main/java/com/slm/slmembed/request/DbConnectionWithQueryRequest.java
@@ -0,0 +1,13 @@
+package com.slm.slmembed.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DbConnectionRequest {
+    private String url;
+    private String username;
+    private String password;
+    private String dbType;
+}
